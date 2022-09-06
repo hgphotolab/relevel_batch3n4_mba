1f9e1e017044fc61437e99e953710742ab672183
hgphotolab <hinanshu.gusain@gmail.com>
2022-09-06 14:13:05 +0530

    changes in utils

--- models/theatre.model.js
+++ models/theatre.model.js
@@ -1,32 +1,35 @@
-const  mongoose = require("mongoose");
+const mongoose = require("mongoose");
+const constants = require("../utils/constants");
+
 
 const theatreSchema = new mongoose.Schema({
 
-    name : {
-        type : String,
-        required : true
+    name: {
+        type: String,
+        required: true
     },
-    description : {
-        type : String,
-        required : true
+    description: {
+        type: String,
+        required: true
     },
-    city : {
-        type : String,
-        required : true
+    city: {
+        type: String,
+        required: true
     },
-    pinCode : {
-        type : Number,
-        required : true
+    pinCode: {
+        type: Number,
+        required: true
     },
-    showTypes : {
-        type : [String],
-        required : true
+    showTypes: {
+        type: [String],
+        required: true,
         //TODO : MORNING | NOON | EVENING | NIGHT
+        enum: [constants.showTypes.morning, constants.showTypes.noon, constants.showTypes.evening, constants.showTypes.night]
     },
-    numberOfSeats : {
-        type : Number,
-        required : true
+    numberOfSeats: {
+        type: Number,
+        required: true
     }
-},{ timestamps : true , versionKey : false});
+}, { timestamps: true, versionKey: false });
 
-module.exports = mongoose.model("Theatre" , theatreSchema);
+module.exports = mongoose.model("Theatre", theatreSchema);
